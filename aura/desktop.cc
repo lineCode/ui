lineCode/ui

commit bbf343b263c6e1b9d146422a8fb9802998582700

Adds support for fullscreening the desktop host on Windows. F11.
BUG=none
TEST=none
Review URL: http://codereview.chromium.org/8440052

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108473 0039d316-1c4b-4281-b951-d872f2087c98

Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
Cr-Mirrored-Commit: 8a1527ba318b957ba41c484d3c45eaae93ec35ff

aura/desktop.cc
@@ -159,6 +159,50 @@ void GetEventFiltersToNotify(Window* target, EventFilters* filters) {
   }
 }
 
+bool MaybeFullScreen(DesktopHost* host, KeyEvent* event) {
+  if (event->key_code() == ui::VKEY_F11) {
+    host->ToggleFullScreen();
+    return true;
+  }
+  return false;
+}
+
+bool MaybeRotate(Desktop* desktop, KeyEvent* event) {
+  if ((event->flags() & ui::EF_SHIFT_DOWN) &&
+      (event->flags() & ui::EF_ALT_DOWN)) {
+    bool should_rotate = true;
+    int new_degrees = 0;
+    switch (event->key_code()) {
+      case ui::VKEY_UP: new_degrees = 0; break;
+      case ui::VKEY_DOWN: new_degrees = 180; break;
+      case ui::VKEY_RIGHT: new_degrees = 90; break;
+      case ui::VKEY_LEFT: new_degrees = -90; break;
+      default: should_rotate = false; break;
+    }
+
+    if (should_rotate) {
+      float rotation = 0.0f;
+      int degrees = 0;
+      const ui::Transform& transform = desktop->layer()->GetTargetTransform();
+      if (ui::InterpolatedTransform::FactorTRS(transform,
+                                               NULL, &rotation, NULL))
+        degrees = NormalizeAngle(new_degrees - SymmetricRound(rotation));
+
+      if (degrees != 0) {
+        desktop->layer()->GetAnimator()->set_preemption_strategy(
+            ui::LayerAnimator::REPLACE_QUEUED_ANIMATIONS);
+        scoped_ptr<ui::LayerAnimationSequence> screen_rotation(
+            new ui::LayerAnimationSequence(new ScreenRotation(degrees)));
+        screen_rotation->AddObserver(desktop);
+        desktop->layer()->GetAnimator()->ScheduleAnimation(
+            screen_rotation.release());
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 }  // namespace
 
 Desktop* Desktop::instance_ = NULL;
@@ -279,38 +323,10 @@ bool Desktop::DispatchMouseEvent(MouseEvent* event) {
 
 bool Desktop::DispatchKeyEvent(KeyEvent* event) {
 #if !defined(NDEBUG)
-  if (event->type() == ui::ET_KEY_PRESSED &&
-      (event->flags() & ui::EF_SHIFT_DOWN) &&
-      (event->flags() & ui::EF_ALT_DOWN)) {
-
-    bool should_rotate = true;
-    int new_degrees = 0;
-    switch (event->key_code()) {
-      case ui::VKEY_UP: new_degrees = 0; break;
-      case ui::VKEY_DOWN: new_degrees = 180; break;
-      case ui::VKEY_RIGHT: new_degrees = 90; break;
-      case ui::VKEY_LEFT: new_degrees = -90; break;
-      default: should_rotate = false; break;
-    }
-
-    if (should_rotate) {
-      float rotation = 0.0f;
-      int degrees = 0;
-      const ui::Transform& transform = layer()->GetTargetTransform();
-      if (ui::InterpolatedTransform::FactorTRS(transform,
-                                               NULL, &rotation, NULL))
-        degrees = NormalizeAngle(new_degrees - SymmetricRound(rotation));
-
-      if (degrees != 0) {
-        layer()->GetAnimator()->set_preemption_strategy(
-            ui::LayerAnimator::REPLACE_QUEUED_ANIMATIONS);
-        scoped_ptr<ui::LayerAnimationSequence> screen_rotation(
-            new ui::LayerAnimationSequence(new ScreenRotation(degrees)));
-        screen_rotation->AddObserver(this);
-        layer()->GetAnimator()->ScheduleAnimation(screen_rotation.release());
-        return true;
-      }
-    }
+  // TODO(beng): replace this hack with global keyboard event handlers.
+  if (event->type() == ui::ET_KEY_PRESSED) {
+    if (MaybeFullScreen(host_.get(), event) || MaybeRotate(this, event))
+      return true;
   }
 #endif
 

aura/desktop_host.h
@@ -43,6 +43,9 @@ class DesktopHost : public MessageLoop::Dispatcher {
   // Shows the DesktopHost.
   virtual void Show() = 0;
 
+  // Toggles the host's full screen state.
+  virtual void ToggleFullScreen() = 0;
+
   // Gets/Sets the size of the DesktopHost.
   virtual gfx::Size GetSize() const = 0;
   virtual void SetSize(const gfx::Size& size) = 0;

aura/desktop_host_linux.cc
@@ -234,6 +234,7 @@ class DesktopHostLinux : public DesktopHost {
   virtual void SetDesktop(Desktop* desktop) OVERRIDE;
   virtual gfx::AcceleratedWidget GetAcceleratedWidget() OVERRIDE;
   virtual void Show() OVERRIDE;
+  virtual void ToggleFullScreen() OVERRIDE;
   virtual gfx::Size GetSize() const OVERRIDE;
   virtual void SetSize(const gfx::Size& size) OVERRIDE;
   virtual void SetCursor(gfx::NativeCursor cursor_type) OVERRIDE;
@@ -421,6 +422,10 @@ gfx::AcceleratedWidget DesktopHostLinux::GetAcceleratedWidget() {
 
 void DesktopHostLinux::Show() {
   XMapWindow(xdisplay_, xwindow_);
+}
+
+void DesktopHostLinux::ToggleFullScreen() {
+  NOTIMPLEMENTED();
 }
 
 gfx::Size DesktopHostLinux::GetSize() const {

aura/desktop_host_win.cc
@@ -112,7 +112,11 @@ gfx::Size DesktopHost::GetNativeDisplaySize() {
                    GetSystemMetrics(SM_CYSCREEN));
 }
 
-DesktopHostWin::DesktopHostWin(const gfx::Rect& bounds) : desktop_(NULL) {
+DesktopHostWin::DesktopHostWin(const gfx::Rect& bounds)
+    : desktop_(NULL),
+      fullscreen_(false),
+      saved_window_style_(0),
+      saved_window_ex_style_(0) {
   Init(NULL, bounds);
   SetWindowText(hwnd(), L"aura::Desktop!");
 }
@@ -137,6 +141,38 @@ gfx::AcceleratedWidget DesktopHostWin::GetAcceleratedWidget() {
 
 void DesktopHostWin::Show() {
   ShowWindow(hwnd(), SW_SHOWNORMAL);
+}
+
+void DesktopHostWin::ToggleFullScreen() {
+  gfx::Rect target_rect;
+  if (!fullscreen_) {
+    fullscreen_ = true;
+    saved_window_style_ = GetWindowLong(hwnd(), GWL_STYLE);
+    saved_window_ex_style_ = GetWindowLong(hwnd(), GWL_EXSTYLE);
+    GetWindowRect(hwnd(), &saved_window_rect_);
+    SetWindowLong(hwnd(), GWL_STYLE,
+                  saved_window_style_ & ~(WS_CAPTION | WS_THICKFRAME));
+    SetWindowLong(hwnd(), GWL_EXSTYLE,
+                  saved_window_ex_style_ & ~(WS_EX_DLGMODALFRAME |
+                      WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE | WS_EX_STATICEDGE));
+
+    MONITORINFO mi;
+    mi.cbSize = sizeof(mi);
+    GetMonitorInfo(MonitorFromWindow(hwnd(), MONITOR_DEFAULTTONEAREST), &mi);
+    target_rect = mi.rcMonitor;
+  } else {
+    fullscreen_ = false;
+    SetWindowLong(hwnd(), GWL_STYLE, saved_window_style_);
+    SetWindowLong(hwnd(), GWL_EXSTYLE, saved_window_ex_style_);
+    target_rect = saved_window_rect_;
+  }
+  SetWindowPos(hwnd(),
+               NULL,
+               target_rect.x(),
+               target_rect.y(),
+               target_rect.width(),
+               target_rect.height(),
+               SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
 }
 
 gfx::Size DesktopHostWin::GetSize() const {

aura/desktop_host_win.h
@@ -24,6 +24,7 @@ class DesktopHostWin : public DesktopHost, public ui::WindowImpl {
   virtual void SetDesktop(Desktop* desktop) OVERRIDE;
   virtual gfx::AcceleratedWidget GetAcceleratedWidget() OVERRIDE;
   virtual void Show() OVERRIDE;
+  virtual void ToggleFullScreen() OVERRIDE;
   virtual gfx::Size GetSize() const OVERRIDE;
   virtual void SetSize(const gfx::Size& size) OVERRIDE;
   virtual void SetCursor(gfx::NativeCursor cursor) OVERRIDE;
@@ -56,6 +57,11 @@ class DesktopHostWin : public DesktopHost, public ui::WindowImpl {
   void OnSize(UINT param, const CSize& size);
 
   Desktop* desktop_;
+
+  bool fullscreen_;
+  RECT saved_window_rect_;
+  DWORD saved_window_style_;
+  DWORD saved_window_ex_style_;
 
   DISALLOW_COPY_AND_ASSIGN(DesktopHostWin);
 };

commit 1e7de8db67ae4700e547b3ac2f973f910b253df7

Fix aura release build
Broken by r108473

BUG=None
TEST=Builds with use_aura=1 in release

Review URL: http://codereview.chromium.org/8475014

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108919 0039d316-1c4b-4281-b951-d872f2087c98

Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
Cr-Mirrored-Commit: eea61f21716ce9dca63a4a8693514e07f8bbf230

aura/desktop.cc
@@ -143,6 +143,7 @@ void GetEventFiltersToNotify(Window* target, EventFilters* filters) {
   }
 }
 
+#if !defined(NDEBUG)
 bool MaybeFullScreen(DesktopHost* host, KeyEvent* event) {
   if (event->key_code() == ui::VKEY_F11) {
     host->ToggleFullScreen();
@@ -184,6 +185,7 @@ bool MaybeRotate(Desktop* desktop, KeyEvent* event) {
   }
   return false;
 }
+#endif
 
 }  // namespace
 